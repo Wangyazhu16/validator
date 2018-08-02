d745648b4f2f3c22c3c92635ef17d4c09137a05e
wangyazhu <wangyazhu@dankegongyu.com>
2018-08-02 10:23:35 +0800

    增加提示

--- main.R
+++ main.R
@@ -3,10 +3,13 @@ source("header.R")
 #' @post /validator
 function(req) {
   
+  cli::cat_line("连接MongoDB数据库...", col = "yellow")
+  
   rd <- est_mongo_conn("Mongo")
   
   dat <- rd$find()
   
-  nrow(dat) %>% print()
+  paste("共有", nrow(dat), "条数据") %>% 
+    cli::cat_boxx(col = "green")
   
 }
